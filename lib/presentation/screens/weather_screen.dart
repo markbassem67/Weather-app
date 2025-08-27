b7a19e77a8a0a9468d83d30866c06ab93b703e1e
markbassem67 <itsbeso67@gmail.com>
2025-08-28 01:01:52 +0300

    .

--- lib/presentation/screens/weather_screen.dart
+++ lib/presentation/screens/weather_screen.dart
@@ -77,7 +77,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
               Scaffold(
                 backgroundColor: Colors.transparent,
                 appBar: AppBar(
-                  // actions: [popupMenu.customPopupMenuWidget()], // add later 
+                  // actions: [popupMenu.customPopupMenuWidget()], // add later
                   backgroundColor: Colors.transparent,
                   elevation: 0.0,
                 ),
